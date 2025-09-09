678bb9d579455d62d848e33a11d88942a0346478
andraos <juddy.andraos@etu.u-paris.fr>
2025-09-09 17:02:59 +0200

    premier travail

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
 import { Component, signal } from '@angular/core';
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
+import { ColorPicker } from "./color-picker/color-picker";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, Footer, ColorPicker],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('juddyanthonyTP1');
+  public bgColor = "white";
+
+  updateColor(newColor:string){
+    this.bgColor = newColor;
+  }
 }
---
+++ src/app/color-picker/color-picker.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-color-picker',
+  imports: [],
+  templateUrl: './color-picker.html',
+  styleUrl: './color-picker.scss'
+})
+export class ColorPicker {
+  @Output() color = new EventEmitter <string>();
+
+  changeColor(newColor:string){
+    this.color.emit(newColor);
+  }
+}
